19c5c25a3d28cbe1387e5db8c83e50b86191b239
camilo <camiloa17@gmail.com>
2024-03-02 14:50:11 -0800

    added more routes

--- handlers.go
+++ handlers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"net/http"
 )
@@ -10,4 +11,27 @@ func healthHandler(w http.ResponseWriter, r *http.Request) {
 
 	w.WriteHeader(http.StatusOK)
 	io.WriteString(w, http.StatusText(http.StatusOK))
+}
+
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	blah := `
+	<html>
+
+<body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited %d times!</p>
+</body>
+
+</html>
+	`
+	w.WriteHeader(http.StatusOK)
+	io.WriteString(w, fmt.Sprintf(blah, cfg.fileserverHit))
+}
+
+func (cfg *apiConfig) metricsResetHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	cfg.fileserverHit = 0
+	w.WriteHeader(http.StatusOK)
+	io.WriteString(w, "Hits reset to 0")
 }
--- main.go
+++ main.go
@@ -3,20 +3,59 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
+
+type apiConfig struct {
+	fileserverHit int
+}
 
 func main() {
 	const port = "8080"
-	const publicFilePath = "./public"
-	mux := http.NewServeMux()
-	mux.HandleFunc("/healthz", healthHandler)
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(publicFilePath))))
+	apiConfig := apiConfig{
+		fileserverHit: 0,
+	}
+	r := chi.NewRouter()
+	r.Use(middleware.CleanPath)
+	r.Use(middleware.StripSlashes)
+	r.Use(middlewareCors)
+	r.Route("/api", func(r chi.Router) {
+		r.Get("/healthz", healthHandler)
+		r.Get("/reset", apiConfig.metricsResetHandler)
+	})
+	r.Route("/admin", func(r chi.Router) {
+		r.Get("/metrics", apiConfig.metricsHandler)
+	})
 
-	corsMux := middlewareCors(mux)
+	r.Group(func(r chi.Router) {
+		r.Use(apiConfig.middlewareMetricsInc)
+		workDir, err := os.Getwd()
+		if err != nil {
+			log.Panicln(err)
+		}
+		fileServeDict := http.Dir(filepath.Join(workDir, "public"))
+		r.Get("/app", func(w http.ResponseWriter, r *http.Request) {
+			rctx := chi.RouteContext(r.Context())
+			pathPrefix := strings.TrimSuffix(rctx.RoutePattern(), "/*")
+			fs := http.StripPrefix(pathPrefix, http.FileServer(fileServeDict))
+			fs.ServeHTTP(w, r)
+		})
+		r.Get("/app/*", func(w http.ResponseWriter, r *http.Request) {
+			rctx := chi.RouteContext(r.Context())
+			pathPrefix := strings.TrimSuffix(rctx.RoutePattern(), "/*")
+			fs := http.StripPrefix(pathPrefix, http.FileServer(fileServeDict))
+			fs.ServeHTTP(w, r)
+		})
+	})
 
 	srv := &http.Server{
 		Addr:    ":" + port,
-		Handler: corsMux,
+		Handler: r,
 	}
 
 	log.Fatal(srv.ListenAndServe())
